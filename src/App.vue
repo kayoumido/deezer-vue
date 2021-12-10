f4fc743f83b27b9882ac594929e9ed04a26778e9
Doran Kayoumi <dorankayoumi@gmail.com>
2021-12-10 12:46:10 +0100

    Add global styles

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,51 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,
+body {
+  height: 100%;
+  font-family: 'Noto Sans', sans-serif;
+  font-size: 16px;
+  line-height: 1.5;
+}
+
+:root {
+  --grey-400: #9CA3AF;
+  --grey-200: #E5E7EB;
+  --grey-100: #F3F4F6;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+}
+
+.body--small {
+  font-size: 0.875rem;
+  line-height: 1.5;
+}
+
+.title {
+  font-weight: bold;
+  font-size: 3rem;
+  line-height: 1.25;
+}
+
+.subtitle {
+  font-weight: bold;
+}
+
+.subtitle--small {
+  font-weight: bold;
+  font-size: 0.875rem;
+}
+
+.overline {
+  font-size: 0.75rem;
 }
 </style>
