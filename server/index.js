e8d525900b008c901c47aac17e049b6033f53edd
Doran Kayoumi <dorankayoumi@gmail.com>
2021-12-10 09:41:08 +0100

    Update server to use axios

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const https = require('https');
+const axios = require('axios');
 
 const deezerAPIURL = 'https://api.deezer.com';
 // Deezer endpoints
@@ -11,34 +11,22 @@ export default (app) => {
   app.use(express.json());
   
   app.get('/playlists', (req, res) => {
-    https.get(`${deezerAPIURL}/chart/0/playlists`, (resp) => {
-      let data = '';
-
-      resp.on('data', (chunk) => {
-        data += chunk;
+    axios.get(`${deezerAPIURL}/chart/0/playlists`)
+      .then(response => {
+        res.json(response.data.data);
+      })
+      .catch(err => {
+        console.error(err);
       });
-
-      resp.on('end', () => {
-        res.json(JSON.parse(data).data);
-      });
-    }).on("error", (err) => {
-      console.log(`Error: ${err.message}`);
-    });
   });
   
   app.get('/playlists/:id', (req, res) => {
-    https.get(`${deezerAPIURL}/playlist/${req.params.id}`, (resp) => {
-      let data = '';
-
-      resp.on('data', (chunk) => {
-        data += chunk;
+    axios.get(`${deezerAPIURL}/playlist/${req.params.id}`)
+      .then(response => {
+        res.json(JSON.parse(response.data));
+      })
+      .catch(err => {
+        console.error(err);
       });
-
-      resp.on('end', () => {
-        res.json(JSON.parse(data));
-      });
-    }).on("error", (err) => {
-      console.log(`Error: ${err.message}`);
-    });
   });
 }
