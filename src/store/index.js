ed0823801663408922004b5741b8fa7d78407f96
Doran Kayoumi <dorankayoumi@gmail.com>
2021-12-11 22:27:59 +0100

    Add queue to store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,25 @@ import Vue from "vue";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    queue: [],
+  },
+  mutations: {
+    addTrackToQueue(state, track) {
+      state.queue.push(track);
+    }
+  },
+  actions: {
+    addTrackToQueue({ commit }, track) {
+      commit('addTrackToQueue', track);
+    }
+  },
+  getters: {
+    isQueueEmpty(state) {
+      return state.queue.length === 0;
+    },
+    queueSize(state) {
+      return state.queue.length;
+    },
+  },
 });
